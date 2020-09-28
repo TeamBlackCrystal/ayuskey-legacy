ab287e0d18fbd9d0fce2ce86f6d134ea0e746c97
mei23 <m@m544.net>
2020-09-29 07:18:32 +0900

    Streaming接続イベントのログ Resolve #1664

--- src/server/api/streaming.ts
+++ src/server/api/streaming.ts
@@ -8,6 +8,10 @@ import { ParsedUrlQuery } from 'querystring';
 import authenticate from './authenticate';
 import { EventEmitter } from 'events';
 import config from '../../config';
+import rndstr from 'rndstr';
+import Logger from '../../services/logger';
+
+export const streamLogger = new Logger('stream', 'cyan');
 
 module.exports = (server: http.Server) => {
 	// Init websocket server
@@ -20,6 +24,11 @@ module.exports = (server: http.Server) => {
 		const [user, app] = await authenticate(q.i as string);
 
 		const connection = request.accept();
+
+		const connHash = rndstr(8);
+		const connPeer = `${connection?.remoteAddress} (xff=${request?.httpRequest?.headers['x-forwarded-for']})`;
+		const connUser = user ? `${user._id} (${user.username})` : 'anonymous';
+		streamLogger.info(`connect ${connHash} (${connPeer} ${connUser})`);
 
 		let ev: EventEmitter;
 
@@ -53,34 +62,15 @@ module.exports = (server: http.Server) => {
 
 		const main = new MainStreamConnection(connection, ev, user, app);
 
-		// 後方互換性のため
+		//#region 後方互換性のため
 		if (request.resourceURL.pathname !== '/streaming') {
-			main.sendMessageToWsOverride = (type: string, payload: any) => {
-				if (type == 'channel') {
-					type = payload.type;
-					payload = payload.body;
-				}
-				if (type.startsWith('api:')) {
-					type = type.replace('api:', 'api-res:');
-				}
-				connection.send(JSON.stringify({
-					type: type,
-					body: payload
-				}));
-			};
-
-			main.connectChannel(Math.random().toString().substr(2, 8), null,
-				request.resourceURL.pathname === '/' ? 'homeTimeline' :
-				request.resourceURL.pathname === '/local-timeline' ? 'localTimeline' :
-				request.resourceURL.pathname === '/hybrid-timeline' ? 'hybridTimeline' :
-				request.resourceURL.pathname === '/global-timeline' ? 'globalTimeline' : null);
-
-			if (request.resourceURL.pathname === '/') {
-				main.connectChannel(Math.random().toString().substr(2, 8), null, 'main');
-			}
+			streamLogger.warn(`404 ${request.resourceURL.pathname} ${connHash} (${connPeer} ${connUser})`);
+			connection.close(404);
 		}
+		//#endregion 後方互換性のため
 
 		connection.once('close', () => {
+			streamLogger.info(`close ${connHash} (${connPeer} ${connUser})`);
 			ev.removeAllListeners();
 			main.dispose();
 		});
@@ -90,5 +80,5 @@ module.exports = (server: http.Server) => {
 				connection.send('pong');
 			}
 		});
-	});
+	});	// ws on request
 };
