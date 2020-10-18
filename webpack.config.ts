5d3f2ee58067a5eec0461ec9df4d1d9ab4b9d124
MeiMei <30769358+mei23@users.noreply.github.com>
2020-10-19 02:07:04 +0900

    Update loaders (#1754)
    * Update css-loader
    
    * Update stylus-loader, vue-svg-inline-loader
    
    * fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -74,7 +74,9 @@ module.exports = {
 				}, {
 					loader: 'css-loader',
 					options: {
-						modules: true
+						modules: true,
+						esModule: false,
+						url: false,
 					}
 				}, postcss, {
 					loader: 'stylus-loader'
@@ -83,7 +85,11 @@ module.exports = {
 				use: [{
 					loader: 'vue-style-loader'
 				}, {
-					loader: 'css-loader'
+					loader: 'css-loader',
+					options: {
+						url: false,
+						esModule: false
+					}
 				}, postcss, {
 					loader: 'stylus-loader'
 				}]
@@ -93,7 +99,10 @@ module.exports = {
 			use: [{
 				loader: 'vue-style-loader'
 			}, {
-				loader: 'css-loader'
+				loader: 'css-loader',
+				options: {
+					esModule: false,
+				}
 			}, postcss]
 		}, {
 			test: /\.(eot|woff|woff2|svg|ttf)([?]?.*)$/,
