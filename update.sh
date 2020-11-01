83d55e6784d7c7927c01ec7b877a73a9037022d6
yupix <yupi0982@outlook.jp>
2020-11-01 13:51:01 +0900

    chore: shellの利便性向上

--- update.sh
+++ update.sh
@@ -13,4 +13,29 @@ while [[ -z "${db_name}" ]]; do
 	read -r -p ">" db_name
 done
 
+cat <<EOF
+
+本当に以下の設定でよろしいですか?
+ユーザー名: ${db_user}
+データベース名: ${db_name}
+よろしい場合は y キャンセルする場合は n を入力してください
+
+EOF
+while :; do
+	read -r -p ">" finall_answer
+	case ${finall_answer} in
+
+	[yY]|[yY][eE][sS])
+		break
+	;;
+	[nN]|[nN][oO])
+		echo "キャンセルしました"
+		exit 0
+	;;
+	*)
+		echo "y または n を入力してください"
+		;;
+	esac
+done
+
 sudo docker-compose exec db psql -U "${db_user}" -d "${db_name}" -c "ALTER TABLE \"user\" ADD COLUMN \"isLady\" boolean NOT NULL DEFAULT false"
--- update_no_docker.sh
+++ update_no_docker.sh
@@ -2,10 +2,9 @@
 
 echo "コマンドが正常に動作しない場合はfirewallなどに弾かれてる可能性があります。"
 
-echo -e "ユーザー名を入力してください\n自分で変更していない場合は\"postgres\"である可能性が高いです"
-while [[ -z "${db_user}" ]];do
-	read -r -p ">" db_user
-done
+echo -e "ユーザー名を入力してください (デフォルト: postgres)"
+read -r -p ">" db_user
+db_user=${db_user:-postgres}
 
 
 echo "データベース名を入力してください"
@@ -13,4 +12,30 @@ while [[ -z "${db_name}" ]]; do
 	read -r -p ">" db_name
 done
 
+cat <<EOF
+
+本当に以下の設定でよろしいですか?
+ユーザー名: ${db_user}
+データベース名: ${db_name}
+よろしい場合は y キャンセルする場合は n を入力してください
+
+EOF
+while :; do
+	read -r -p ">" finall_answer
+	case ${finall_answer} in
+
+	[yY]|[yY][eE][sS])
+		break
+	;;
+	[nN]|[nN][oO])
+		echo "キャンセルしました"
+		exit 0
+	;;
+	*)
+		echo "y または n を入力してください"
+		;;
+	esac
+done
+
 sudo -u postgres psql -U "${db_user}" -d "${db_name}" -c "ALTER TABLE \"user\" ADD COLUMN \"isLady\" boolean NOT NULL DEFAULT false"
+echo "アップデートが終了しました"
