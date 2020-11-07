974dcf3ee60e1ca6c320c9ea638e7ca2157f437d
mei23 <m@m544.net>
2020-11-08 07:59:31 +0900

    Reduce i18n error

--- src/client/app/common/views/deck/deck.notifications.vue
+++ src/client/app/common/views/deck/deck.notifications.vue
@@ -19,7 +19,7 @@
 	<button class="more" :class="{ fetching: fetchingMoreNotifications }" v-if="moreNotifications" @click="fetchMoreNotifications" :disabled="fetchingMoreNotifications">
 		<template v-if="fetchingMoreNotifications"><fa icon="spinner" pulse fixed-width/></template>{{ fetchingMoreNotifications ? this.$t('@.loading') : this.$t('@.load-more') }}
 	</button>
-	<p class="empty" v-if="notifications.length == 0 && !fetching">{{ $t('empty') }}</p>
+	<p class="empty" v-if="notifications.length == 0 && !fetching">{{ $t('@.empty') }}</p>
 </div>
 </template>
 
