515e5e30d8f74df5e5b85ded09e5d4093ba7b2cc
sousuke0422 <sousuke20xx@gmail.com>
2020-11-15 01:36:50 +0900

    fix: v12のnull問題解決を部分的に適応

--- src/models/repositories/drive-file.ts
+++ src/models/repositories/drive-file.ts
@@ -122,7 +122,8 @@ export class DriveFileRepository extends Repository<DriveFile> {
 			folder: opts.detail && file.folderId ? DriveFolders.pack(file.folderId, {
 				detail: true
 			}) : null,
-			user: opts.withUser ? Users.pack(file.userId!) : null
+			//userId: opts.withUser ? file.userId : null,
+			user: (opts.withUser && file.userId) ? Users.pack(file.userId) : null
 		});
 	}
 
