0490e7e650e6dbc7a5dc5e365d6b472be6a52a07
sousuke0422 <sousuke20xx@gmail.com>
2020-11-22 00:17:17 +0900

    feat: 中身をm544のものに置き換え

--- src/client/app/common/views/components/instance-ticker.vue
+++ src/client/app/common/views/components/instance-ticker.vue
@@ -1,59 +1,77 @@
 <template>
-<div class="hpaizdrt" :style="bg">
-	<img v-if="info.faviconUrl" class="icon" :src="info.faviconUrl"/>
-	<span class="name">{{ info.host }}</span>
-</div>
+<router-link class="instance-info-wrap" v-if="instance != null" :to="`/search?q=${ encodeURIComponent(`host:${instance.host}`) }`">
+	<div class="instance-info" :title="getDetail(instance)" :style="{ background: `linear-gradient(to right, ${themeColor}, rgba(0, 0, 0, 0))` }">
+		<img class="icon" v-if="instance.iconUrl != null" :src="`/proxy/icon.ico?${urlQuery({ url: instance.iconUrl })}`"/>
+		<div class="name">
+			{{ (instance.name && instance.name !== instance.host) ? `${instance.name} (${instance.host})` : `${instance.host}` }}
+		</div>
+	</div>
+</router-link>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
-import { instanceName } from '../../../config';
-export default defineComponent({
-	props: {
-		instance: {
-			type: Object,
-			required: false
-		},
-	},
+import Vue from 'vue';
+import { query as urlQuery } from '../../../../../prelude/url';
+type II = {
+	host?: string;
+	name?: string;
+	softwareName?: string;
+	softwareVersion?: string;
+	iconUrl?: string;
+};
+export default Vue.extend({ 
+	props: ['instance'],
 	data() {
 		return {
-			info: this.instance || {
-				faviconUrl: '/favicon.ico',
-				host: instanceName,
-				themeColor: (document.querySelector('meta[name="theme-color-orig"]') as HTMLMetaElement)?.content
-			}
+			urlQuery
 		}
 	},
 	computed: {
-		bg(): any {
-			const themeColor = this.info.themeColor || '#777777';
-			return {
-				background: `linear-gradient(90deg, ${themeColor}, ${themeColor + '00'})`
-            };
-		}
+		themeColor(): string {
+			if (this.instance.themeColor) {
+				const m = this.instance.themeColor.match(/^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})/);	// TODO: other format
+				const r = parseInt(m[1], 16);
+				const g = parseInt(m[2], 16);
+				const b = parseInt(m[3], 16);
+				return `rgba(${r}, ${g}, ${b}, 0.5)`;
+			} else {
+				return `rgba(64, 64, 255, 0.5)`;
+			}
+		},
+	},
+	methods: {
+		getName(instance: II): string {
+			if (!instance) return 'Unknown';
+			return instance.name ? `${instance.name} (${instance.host})` : `${instance.host}`;
+		},
+		getDetail(instance: II): string {
+			let s = this.getName(instance);
+			if (instance.softwareName) {
+				s += '\n' + (instance.softwareVersion ? `${instance.softwareName} (${instance.softwareVersion})` : `${instance.softwareName})`);
+			}
+			return s;
+		},
 	}
 });
 </script>
 
-<style lang="scss" scoped>
-.hpaizdrt {
-	$height: 1.1rem;
-    
-	height: $height;
-	border-radius: 4px 0 0 4px;
-	overflow: hidden;
-	color: #fff;
-    margin-bottom: 0.3em;
-    //color: var(--noteText);
-	> .icon {
-		height: 100%;
-	}
-	> .name {
-		margin-left: 4px;
-		line-height: $height;
-		font-size: 0.9em;
-		vertical-align: top;
-		//font-weight: bold;
-	}
-}
+<style lang="stylus" scoped>
+	.instance-info-wrap
+		text-decoration none 
+		cursor pointer
+		.instance-info
+			display flex
+			align-items center
+			font-size 0.9em
+			color var(--noteText)
+			border-radius 0.2em
+			margin-bottom 0.3em
+			.icon
+				width 1em
+				height 1em
+				margin-right 0.2em
+			.name
+				overflow hidden
+				white-space nowrap
+				text-overflow ellipsis
 </style>
