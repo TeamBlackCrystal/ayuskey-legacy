ed7adb4669d4104898b9ede50202b8699689692b
sousuke0422 <sousuke20xx@gmail.com>
2020-12-06 12:06:07 +0900

    chore: ちゃんとした方法(?)でnote.textを変更するように

--- migration/1604921883816-NoteText.ts
+++ migration/1604921883816-NoteText.ts
@@ -3,11 +3,11 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class NoteText1604921883816 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "note" ADD IF NOT EXISTS "text" character varying(10240)`);
+				await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "text" TYPE character varying(10240)`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "note" ADD "text" character varying(8192)`);
+				await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "text" TYPE character varying(8192)`, undefined);
     }
 
 }
