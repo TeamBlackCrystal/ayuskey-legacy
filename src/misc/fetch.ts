8e3e25fb2605c5122d63cef701bb0ef41160942c
MeiMei <30769358+mei23@users.noreply.github.com>
2021-01-07 00:16:04 +0900

    download以外でも全体処理タイムアウトさせる #2099 (#2100)

--- src/misc/fetch.ts
+++ src/misc/fetch.ts
@@ -5,8 +5,14 @@ import fetch, { HeadersInit } from 'node-fetch';
 import { HttpProxyAgent } from 'http-proxy-agent';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 import config from '../config';
+import { AbortController } from 'abort-controller';
 
 export async function getJson(url: string, accept = 'application/json, */*', timeout = 10000, headers?: HeadersInit) {
+	const controller = new AbortController();
+	setTimeout(() => {
+		controller.abort();
+	}, 60 * 1000);
+
 	const res = await fetch(url, {
 		headers: Object.assign({
 			'User-Agent': config.userAgent,
@@ -15,6 +21,7 @@ export async function getJson(url: string, accept = 'application/json, */*', tim
 		timeout,
 		size: 10 * 1024 * 1024,
 		agent: getAgentByUrl,
+		signal: controller.signal,
 	});
 
 	if (!res.ok) {
@@ -29,6 +36,11 @@ export async function getJson(url: string, accept = 'application/json, */*', tim
 }
 
 export async function getHtml(url: string, accept = 'text/html, */*', timeout = 10000, headers?: HeadersInit) {
+	const controller = new AbortController();
+	setTimeout(() => {
+		controller.abort();
+	}, 60 * 1000);
+
 	const res = await fetch(url, {
 		headers: Object.assign({
 			'User-Agent': config.userAgent,
@@ -36,6 +48,7 @@ export async function getHtml(url: string, accept = 'text/html, */*', timeout = 
 		}, headers || {}),
 		timeout,
 		agent: getAgentByUrl,
+		signal: controller.signal,
 	});
 
 	if (!res.ok) {
