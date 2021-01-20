9281921fc6c9fdb0b767c4ffd04806e17ee6e126
mei23 <m@m544.net>
2021-01-21 08:57:33 +0900

    AP優先は/users/:userのみ

--- src/server/activitypub.ts
+++ src/server/activitypub.ts
@@ -44,10 +44,11 @@ function inbox(ctx: Router.RouterContext) {
 const ACTIVITY_JSON = 'application/activity+json; charset=utf-8';
 const LD_JSON = 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"; charset=utf-8';
 
-function isActivityPubReq(ctx: Router.RouterContext) {
+function isActivityPubReq(ctx: Router.RouterContext, preferAp = false) {
 	ctx.response.vary('Accept');
-	// 明確にAPよりHTMLが優先されていなければAPとみなす
-	const accepted = ctx.accepts(ACTIVITY_JSON, LD_JSON, 'html');
+	const accepted = preferAp
+		? ctx.accepts(ACTIVITY_JSON, LD_JSON, 'html')
+		: ctx.accepts('html', ACTIVITY_JSON, LD_JSON);
 	return typeof accepted === 'string' && !accepted.match(/html/);
 }
 
@@ -163,7 +164,7 @@ async function userInfo(ctx: Router.RouterContext, user: User | undefined) {
 }
 
 router.get('/users/:user', async (ctx, next) => {
-	if (!isActivityPubReq(ctx)) return await next();
+	if (!isActivityPubReq(ctx, true)) return await next();
 
 	const userId = ctx.params.user;
 
