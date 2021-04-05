150fc1a3ee93a9018104e64d499ff9c8ed6c8d6d
sousuke0422 <sousuke20xx@gmail.com>
2021-04-05 12:14:29 +0900

    feat!: 絵文字apiの仕様変更
    BREAKING CHANGE: listの仕様が変更され、ローカル絵文字専用になりました。従来の使い方をする場合list-remoteを使用する必要があります。

--- src/server/api/endpoints/admin/emoji/list.ts
+++ src/server/api/endpoints/admin/emoji/list.ts
@@ -1,11 +1,14 @@
 import $ from 'cafy';
 import define from '../../../define';
 import { Emojis } from '../../../../../models';
-import { toPunyNullable } from '../../../../../misc/convert-host';
+import { makePaginationQuery } from '../../../common/make-pagination-query';
+import { ID } from '../../../../../misc/cafy-id';
+import { Emoji } from '../../../../../models/entities/emoji';
 
 export const meta = {
 	desc: {
-		'ja-JP': 'カスタム絵文字を取得します。'
+		'ja-JP': 'カスタム絵文字一覧を取得します。',
+		'en-US': 'List custom emojis.'
 	},
 
 	tags: ['admin'],
@@ -14,30 +17,93 @@ export const meta = {
 	requireModerator: true,
 
 	params: {
-		host: {
+		query: {
 			validator: $.optional.nullable.str,
 			default: null as any
+		},
+
+		limit: {
+			validator: $.optional.num.range(1, 100),
+			default: 10
+		},
+
+		sinceId: {
+			validator: $.optional.type(ID),
+		},
+
+		untilId: {
+			validator: $.optional.type(ID),
+		}
+	},
+
+	res: {
+		type: 'array' as const,
+		optional: false as const, nullable: false as const,
+		items: {
+			type: 'object' as const,
+			optional: false as const, nullable: false as const,
+			properties: {
+				id: {
+					type: 'string' as const,
+					optional: false as const, nullable: false as const,
+					format: 'id',
+					description: 'The unique identifier for this Emoji.'
+				},
+				aliases: {
+					type: 'array' as const,
+					optional: false as const, nullable: false as const,
+					description: 'List to make it easier to be displayed as a candidate when entering emoji.',
+					items: {
+						type: 'string' as const,
+						optional: false as const, nullable: false as const
+					}
+				},
+				name: {
+					type: 'string' as const,
+					optional: false as const, nullable: false as const,
+					description: 'Official name of custom emoji.'
+				},
+				category: {
+					type: 'string' as const,
+					optional: false as const, nullable: true as const,
+					description: 'Names categorized in the emoji list.'
+				},
+				host: {
+					type: 'string' as const,
+					optional: false as const, nullable: true as const,
+					description: 'If it is another server, the FQDN will be returned here.'
+				},
+				url: {
+					type: 'string' as const,
+					optional: false as const, nullable: false as const,
+					description: 'Image URL of emoji.'
+				}
+			}
 		}
 	}
 };
 
 export default define(meta, async (ps) => {
-	const emojis = await Emojis.find({
-		where: {
-			host: toPunyNullable(ps.host)
-		},
-		order: {
-			category: 'ASC',
-			name: 'ASC'
-		}
-	});
+	const q = makePaginationQuery(Emojis.createQueryBuilder('emoji'), ps.sinceId, ps.untilId)
+		.andWhere(`emoji.host IS NULL`);
 
-	return emojis.map(e => ({
-		id: e.id,
-		name: e.name,
-		category: e.category,
-		aliases: e.aliases,
-		host: e.host,
-		url: e.url
-	}));
+	let emojis: Emoji[];
+
+	if (ps.query) {
+		//q.andWhere('emoji.name ILIKE :q', { q: `%${ps.query}%` });
+		//const emojis = await q.take(ps.limit!).getMany();
+
+		emojis = await q.getMany();
+
+		emojis = emojis.filter(emoji =>
+			emoji.name.includes(ps.query!) ||
+			emoji.aliases.some(a => a.includes(ps.query!)) ||
+			emoji.category?.includes(ps.query!));
+
+		emojis.splice(ps.limit! + 1);
+	} else {
+		emojis = await q.take(ps.limit!).getMany();
+	}
+
+	return Emojis.packMany(emojis);
 });
