a8d074da6d786305f7eb4c8a76a94f18e308c9c7
sousuke0422 <sousuke20xx@gmail.com>
2021-05-30 21:08:14 +0900

    Revert "feat: nyaizeを最新のものに(v12より移植)"
    This reverts commit fe668ea10ebf6fe9d8188be6a6a409d314b9aa68.

--- src/misc/nyaize.ts
+++ src/misc/nyaize.ts
@@ -3,13 +3,9 @@ export function nyaize(text: string): string {
 		// ja-JP
 		.replace(/な/g, 'にゃ').replace(/ナ/g, 'ニャ').replace(/ﾅ/g, 'ﾆｬ')
 		// en-US
-		.replace(/(?<=n)a/gi, x => x === 'A' ? 'YA' : 'ya')
-		.replace(/(?<=morn)ing/gi, x => x === 'ING' ? 'YAN' : 'yan')
-		.replace(/(?<=every)one/gi, x => x === 'ONE' ? 'NYAN' : 'nyan')
+		.replace(/morning/gi, 'mornyan').replace(/everyone/gi, 'everynyan')
 		// ko-KR
 		.replace(/[나-낳]/g, match => String.fromCharCode(
-			match.charCodeAt(0)! + '냐'.charCodeAt(0) - '나'.charCodeAt(0)
-		))
-		.replace(/(다$)|(다(?=\.))|(다(?= ))|(다(?=!))|(다(?=\?))/gm, '다냥')
-		.replace(/(야(?=\?))|(야$)|(야(?= ))/gm, '냥');
+			match.codePointAt(0)! + '냐'.charCodeAt(0) - '나'.charCodeAt(0)
+		));
 }
