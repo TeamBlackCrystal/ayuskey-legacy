d6d4cdfc2907fb3e9bba221d2c9903e59b84c35c
sousuke0422 <sousuke20xx@gmail.com>
2021-07-02 00:08:31 +0900

    feat: fxxx FLoC

--- src/server/index.ts
+++ src/server/index.ts
@@ -55,6 +55,12 @@ if (config.url.startsWith('https') && !config.disableHsts) {
 	});
 }
 
+// FLoC
+app.use(async (ctx, next) => {
+	ctx.set('Permissions-Policy', 'interest-cohort=()');
+	await next();
+});
+
 app.use(mount('/api', apiServer));
 app.use(mount('/files', require('./file')));
 app.use(mount('/proxy', require('./proxy')));
