b5d963ea6fad6036bd1dfb2fc6cbc13f90215548
sousuke0422 <sousuke20xx@gmail.com>
2021-07-07 23:27:14 +0900

    feat: esやsonicなしで検索できるように

--- src/server/api/endpoints/notes/search.ts
+++ src/server/api/endpoints/notes/search.ts
@@ -5,6 +5,10 @@ import { ApiError } from '../../error';
 import { Notes } from '../../../../models';
 import { In } from 'typeorm';
 import { ID } from '../../../../misc/cafy-id';
+import config from '../../../../config';
+import { makePaginationQuery } from '../../common/make-pagination-query';
+import { generateVisibilityQuery } from '../../common/generate-visibility-query';
+import { generateMutedUserQuery } from '../../common/generate-muted-user-query';
 
 export const meta = {
 	desc: {
@@ -19,6 +23,14 @@ export const meta = {
 	params: {
 		query: {
 			validator: $.str
+		},
+
+		sinceId: {
+			validator: $.optional.type(ID),
+		},
+
+		untilId: {
+			validator: $.optional.type(ID),
 		},
 
 		limit: {
@@ -39,7 +51,12 @@ export const meta = {
 		userId: {
 			validator: $.optional.nullable.type(ID),
 			default: null
-		}
+		},
+
+		channelId: {
+			validator: $.optional.nullable.type(ID),
+			default: null
+		},
 	},
 
 	res: {
@@ -64,25 +81,84 @@ export const meta = {
 };
 
 export default define(meta, async (ps, me) => {
-	if (searchClient == null)
-		throw new ApiError(meta.errors.searchingNotAvailable);
+	if (searchClient == null) {
+		if (config.searchFalse == null) config.searchFalse = false;
+		if (config.searchFalse) throw new ApiError(meta.errors.searchingNotAvailable);
 
-	const hits = await searchClient.search(ps.query, {
-		userHost: ps.host,
-		userId: ps.userId
-	});
+		const query = makePaginationQuery(Notes.createQueryBuilder('note'), ps.sinceId, ps.untilId);
+		const sinceRegex = /since:([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])) /g;
+		const untilRegex = /until:([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])) /g;
+		const hostRegex = /host:([a-zA-Z0-9.-]+) /g;
 
-	if (hits.length === 0) return [];
-
-	// Fetch found notes
-	const notes = await Notes.find({
-		where: {
-			id: In(hits)
-		},
-		order: {
-			id: -1
+		if (ps.userId) {
+			query.andWhere('note.userId = :userId', { userId: ps.userId });
+		} else if (ps.channelId) {
+			query.andWhere('note.channelId = :channelId', { channelId: ps.channelId });
 		}
-	});
 
-	return await Notes.packMany(notes, me);
+		if (sinceRegex.test(ps.query)) {
+			query.andWhere('note.createdAt > :since', {since: `${RegExp.$1}`});
+			ps.query = ps.query.replaceAll(sinceRegex, '');
+		}
+		if (untilRegex.test(ps.query)) {
+			query.andWhere('note.createdAt < :until', {until: `${RegExp.$1} 23:59:59`});
+			ps.query = ps.query.replaceAll(untilRegex, '');
+		}
+		if (hostRegex.test(ps.query)) {
+			if (RegExp.$1 === 'local') {
+				query.andWhere('note.userHost IS NULL');
+			} else {
+				query.andWhere('note.userHost = :host', {host: `${RegExp.$1}`});
+			}
+			ps.query = ps.query.replaceAll(hostRegex, '');
+		}
+
+		ps.query = ps.query.replaceAll(/\s\s+/g, ' ');
+
+		if (config.db.pgroonga == null) config.db.pgroonga = false;
+		if (config.db.pgroonga) {
+			query
+				.andWhere('note.text &@~ :q', { q: `${ps.query}` })
+				.innerJoinAndSelect('note.user', 'user')
+				.leftJoinAndSelect('note.reply', 'reply')
+				.leftJoinAndSelect('note.renote', 'renote')
+				.leftJoinAndSelect('reply.user', 'replyUser')
+				.leftJoinAndSelect('renote.user', 'renoteUser');
+		} else {
+			query
+				.andWhere('note.text ILIKE :q', { q: `%${ps.query}%` })
+				.innerJoinAndSelect('note.user', 'user')
+				.leftJoinAndSelect('note.reply', 'reply')
+				.leftJoinAndSelect('note.renote', 'renote')
+				.leftJoinAndSelect('reply.user', 'replyUser')
+				.leftJoinAndSelect('renote.user', 'renoteUser');
+		}
+
+		generateVisibilityQuery(query, me);
+		if (me) generateMutedUserQuery(query, me);
+
+		const notes = await query.take(ps.limit!).getMany();
+
+		return await Notes.packMany(notes, me);
+	} else {
+
+		const hits = await searchClient.search(ps.query, {
+			userHost: ps.host,
+			userId: ps.userId
+		});
+
+		if (hits.length === 0) return [];
+
+		// Fetch found notes
+		const notes = await Notes.find({
+			where: {
+				id: In(hits)
+			},
+			order: {
+				id: -1
+			}
+		});
+
+		return await Notes.packMany(notes, me);
+}
 });
