0e6498c0847499ade358dcc2359012b12e30062f
sousuke0422 <sousuke20xx@gmail.com>
2021-08-11 01:49:26 +0900

    chore: remove log

--- src/client/app/desktop/views/components/ui.header.account.vue
+++ src/client/app/desktop/views/components/ui.header.account.vue
@@ -175,7 +175,6 @@ export default Vue.extend({
 		},
 	},
 	mounted() {
-		console.log(this.$store.state.device.darkmode)
 		if (this.$store.state.device.darkmode == true) { // ダークテーマが有効の場合のみblurを強化
 			this.$set(this.menublur_style, 'backdrop-filter', 'blur(2em)');
 		} else {
