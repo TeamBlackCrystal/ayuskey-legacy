30523e494acf8a756474ff39c23b301053550395
sousuke0422 <sousuke20xx@gmail.com>
2021-08-19 16:46:16 +0900

    fix: テーブル間違えている

--- migration/1629287783046-user-profile-length.ts
+++ migration/1629287783046-user-profile-length.ts
@@ -3,12 +3,12 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class userProfileLength1629287783046 implements MigrationInterface {
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`ALTER TABLE "user_profile" ALTER COLUMN "name" TYPE character varying(512)`, undefined);
+		await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "name" TYPE character varying(512)`, undefined);
 		await queryRunner.query(`ALTER TABLE "user_profile" ALTER COLUMN "description" TYPE character varying(2048)`, undefined);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`ALTER TABLE "user_profile" ALTER COLUMN "name" TYPE character varying(128)`, undefined);
+		await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "name" TYPE character varying(128)`, undefined);
 		await queryRunner.query(`ALTER TABLE "user_profile" ALTER COLUMN "description" TYPE character varying(2048)`, undefined);
 	}
 
