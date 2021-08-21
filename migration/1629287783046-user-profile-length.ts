32b45d181e5f9759119c840e382f6af122049a93
sousuke0422 <sousuke20xx@gmail.com>
2021-08-21 15:43:45 +0900

    fix: 数字変わってない

--- migration/1629287783046-user-profile-length.ts
+++ migration/1629287783046-user-profile-length.ts
@@ -4,7 +4,7 @@ export class userProfileLength1629287783046 implements MigrationInterface {
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "name" TYPE character varying(512)`, undefined);
-		await queryRunner.query(`ALTER TABLE "user_profile" ALTER COLUMN "description" TYPE character varying(2048)`, undefined);
+		await queryRunner.query(`ALTER TABLE "user_profile" ALTER COLUMN "description" TYPE character varying(8192)`, undefined);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
