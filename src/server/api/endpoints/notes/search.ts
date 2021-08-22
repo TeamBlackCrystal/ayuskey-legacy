d0c3eae2470fa661420ddff0580d0e4def38abb2
sousuke0422 <sousuke20xx@gmail.com>
2021-08-22 19:50:50 +0900

    chore: スペース無しで反応するように

--- src/server/api/endpoints/notes/search.ts
+++ src/server/api/endpoints/notes/search.ts
@@ -86,9 +86,9 @@ export default define(meta, async (ps, me) => {
 		if (config.searchFalse) throw new ApiError(meta.errors.searchingNotAvailable);
 
 		const query = makePaginationQuery(Notes.createQueryBuilder('note'), ps.sinceId, ps.untilId);
-		const sinceRegex = /since:([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])) /g;
-		const untilRegex = /until:([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])) /g;
-		const hostRegex = /host:([a-zA-Z0-9.-]+) /g;
+		const sinceRegex = /since:([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))/g;
+		const untilRegex = /until:([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))/g;
+		const hostRegex = /host:([a-zA-Z0-9.-]+)/g;
 
 		if (ps.userId) {
 			query.andWhere('note.userId = :userId', { userId: ps.userId });
