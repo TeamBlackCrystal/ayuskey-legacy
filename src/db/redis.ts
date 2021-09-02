e197608e5eaf3d6d3a6293d75ecfbc7850e9d51a
sousuke0422 <sousuke20xx@gmail.com>
2021-09-02 16:29:42 +0900

    chore: unixsocket

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -2,15 +2,26 @@ import * as redis from 'redis';
 import config from '../config';
 
 export function createConnection() {
-	return redis.createClient(
-		config.redis.port,
-		config.redis.host,
-		{
-			password: config.redis.pass,
-			prefix: config.redis.prefix,
-			db: config.redis.db || 0
-		}
-	);
+	if (!config.redis.url) { 
+		return redis.createClient(
+			config.redis.port,
+			config.redis.host,
+			{
+				password: config.redis.pass,
+				prefix: config.redis.prefix,
+				db: config.redis.db || 0
+			}
+		);
+	} else {
+		return redis.createClient(
+			config.redis.url,
+			{
+				password: config.redis.pass,
+				prefix: config.redis.prefix,
+				db: config.redis.db || 0
+			}
+		);
+	}
 }
 
 export const subsdcriber = createConnection();
