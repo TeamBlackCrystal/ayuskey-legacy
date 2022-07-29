b4ac10feaf2a37e573574f7ecb0a30c8003505e3
sousuke0422 <sousuke20xx@gmail.com>
2022-07-30 01:49:35 +0900

    feat: unicode 14

--- src/misc/twemoji-base.ts
+++ src/misc/twemoji-base.ts
@@ -1,1 +1,8 @@
-export const twemojiSvgBase = '/twemoji';
+// Official (Slow, no IPv6)
+// export const twemojiSvgBase = 'https://twemoji.maxcdn.com/v/latest/svg';
+
+// Alternative (Fast, with IPv6)
+export const twemojiSvgBase = 'https://cdn.jsdelivr.net/npm/@discordapp/twemoji@14.0.2/dist/svg';
+
+// Self hosting
+//export const twemojiSvgBase = '/twemoji';
