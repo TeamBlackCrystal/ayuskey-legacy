a7bfde28570761cf4dd947f0afc66b28bce223cd
yupix <yupi0982@outlook.jp>
2022-08-02 21:22:59 +0900

    fix: close #1496

--- src/client/app/common/views/components/media-image.vue
+++ src/client/app/common/views/components/media-image.vue
@@ -4,7 +4,7 @@
 	<div class="text">
 		<div>
 			<b><fa :icon="faExclamationTriangle"/> {{ $t('sensitive') }}</b>
-			<span>{{ $t('clickToShow') }}</span>
+			<span>{{ $t('click-to-show') }}</span>
 		</div>
 	</div>
 </div>
