01a6ad206fe33aefcd09fc69027ff8b8f0bdbdbf
sousuke0422 <sousuke20xx@gmail.com>
2022-09-10 01:43:23 +0900

    refactor: define component

--- src/client/app/app.vue
+++ src/client/app/app.vue
@@ -3,10 +3,10 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { defineComponent } from 'vue';
 import { url, lang } from './config';
 
-export default Vue.extend({
+export default defineComponent({
 	computed: {
 		keymap(): any {
 			return {
