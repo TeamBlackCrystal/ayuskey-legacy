7caf5401fb4ea1458f1ad2cbf0996cafc07972f5
sousuke0422 <sousuke20xx@gmail.com>
2022-10-15 22:07:35 +0900

    feat: dynamic port
    本体連携のサンプル

--- packages/next-api/src/main.ts
+++ packages/next-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'node:fs';
+import { Config } from '../../../built/config/types';
+
+const config = yaml.load(
+	readFileSync('../../.config/default.yml', 'utf-8'),
+) as Config;
+
+// TODO: まともにする
+const port = config.port + 100;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	console.log(`port: ${port}`);
+  await app.listen(port);
 }
 bootstrap();
