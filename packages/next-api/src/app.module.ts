b7fb9b677dcea072740b6a28de9b7198bf48d8db
yupix <yupi0982@outlook.jp>
2022-10-16 15:35:00 +0000

    chore: typeormの設定を追加

--- packages/next-api/src/app.module.ts
+++ packages/next-api/src/app.module.ts
@@ -1,11 +1,30 @@
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'node:fs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Config } from '../../../built/config/types';
+
+const config = yaml.load(
+	readFileSync('../../.config/default.yml', 'utf-8'),
+) as Config;
 
 @Module({
-  imports: [NotesModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: config.db.host,
+			port: config.db.port,
+			username: config.db.user,
+			password: config.db.pass,
+			database: config.db.db,
+			entities: ['../../../built/models/entities/**/*.js'],
+		}),
+		NotesModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
