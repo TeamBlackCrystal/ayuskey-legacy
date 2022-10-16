2fb441d6cc8ed2a96b13a59fbc30609c4e63a33f
yupix <yupi0982@outlook.jp>
2022-10-16 14:33:48 +0000

    feat: noteに関するものを追加 #290

--- packages/next-api/src/app.module.ts
+++ packages/next-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/next-api/src/notes/notes.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { NotesService } from './notes.service';
+import { NotesController } from './notes.controller';
+
+@Module({
+  providers: [NotesService],
+  controllers: [NotesController]
+})
+export class NotesModule {}
