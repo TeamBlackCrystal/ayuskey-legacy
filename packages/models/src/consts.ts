bad8b6523c6f0e3c352752e2912e857e3292ec2c
yupix <yupi0982@outlook.jp>
2022-10-17 20:31:01 +0900

    chore: @ayuskey/modelsを使用するように

---
+++ packages/models/src/consts.ts
@@ -0,0 +1,7 @@
+// TODO: ayuskey.jsに移動
+
+export const notificationTypes = ['follow', 'mention', 'reply', 'renote', 'quote', 'reaction', 'pollVote', 'receiveFollowRequest', 'followRequestAccepted', 'groupInvited', 'app'] as const;
+
+export const noteVisibilities = ['public', 'home', 'followers', 'specified'] as const;
+
+export const mutedNoteReasons = ['word', 'manual', 'spam', 'other'] as const;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,30 +1,43 @@
 import { getRepository, getCustomRepository } from 'typeorm';
-import { Announcement } from './entities/announcement';
-import { AnnouncementRead } from './entities/announcement-read';
-import { Instance } from './entities/instance';
-//import { Emoji } from './entities/emoji';
+import {
+	Announcement,
+	AnnouncementRead,
+	NoteWatching,
+	NoteUnread,
+	Poll,
+	PollVote,
+	UserProfile,
+	UserKeypair,
+	AttestationChallenge,
+	UserSecurityKey,
+	UserPublickey,
+	UserListJoining,
+	UserGroupJoining,
+	UserNotePining,
+	UsedUsername,
+	Instance,
+	Meta,
+	SwSubscription,
+	RegistrationTicket,
+	AccessToken,
+	Log,
+	ClipNote,
+	AntennaNote,
+	RegistryItem,
+	ChannelFollowing,
+	ChannelNotePining,
+	PasswordResetRequest
+} from '@ayuskey/models';
 import { EmojiRepository } from './repositories/emoji';
-import { Poll } from './entities/poll';
-import { PollVote } from './entities/poll-vote';
-import { Meta } from './entities/meta';
-import { SwSubscription } from './entities/sw-subscription';
-import { NoteWatching } from './entities/note-watching';
-import { NoteUnread } from './entities/note-unread';
-import { RegistrationTicket } from './entities/registration-tickets';
 import { UserRepository } from './repositories/user';
 import { NoteRepository } from './repositories/note';
 import { DriveFileRepository } from './repositories/drive-file';
 import { DriveFolderRepository } from './repositories/drive-folder';
-import { Log } from './entities/log';
-import { AccessToken } from './entities/access-token';
-import { UserNotePining } from './entities/user-note-pinings';
 import { SigninRepository } from './repositories/signin';
 import { MessagingMessageRepository } from './repositories/messaging-message';
 import { ReversiGameRepository } from './repositories/games/reversi/game';
 import { UserListRepository } from './repositories/user-list';
-import { UserListJoining } from './entities/user-list-joining';
 import { UserGroupRepository } from './repositories/user-group';
-import { UserGroupJoining } from './entities/user-group-joining';
 import { UserGroupInviteRepository } from './repositories/user-group-invite';
 import { FollowRequestRepository } from './repositories/follow-request';
 import { MutingRepository } from './repositories/muting';
@@ -33,31 +46,18 @@ import { NoteReactionRepository } from './repositories/note-reaction';
 import { NotificationRepository } from './repositories/notification';
 import { NoteFavoriteRepository } from './repositories/note-favorite';
 import { ReversiMatchingRepository } from './repositories/games/reversi/matching';
-import { UserPublickey } from './entities/user-publickey';
-import { UserKeypair } from './entities/user-keypair';
 import { AppRepository } from './repositories/app';
 import { FollowingRepository } from './repositories/following';
 import { AbuseUserReportRepository } from './repositories/abuse-user-report';
 import { AuthSessionRepository } from './repositories/auth-session';
-import { UserProfile } from './entities/user-profile';
-import { AttestationChallenge } from './entities/attestation-challenge';
-import { UserSecurityKey } from './entities/user-security-key';
 import { HashtagRepository } from './repositories/hashtag';
 import { PageRepository } from './repositories/page';
 import { PageLikeRepository } from './repositories/page-like';
 import { ModerationLogRepository } from './repositories/moderation-logs';
-import { UsedUsername } from './entities/used-username';
 import { ClipRepository } from './repositories/clip';
-import { ClipNote } from './entities/clip-note';
 import { AntennaRepository } from './repositories/antenna';
-import { AntennaNote } from './entities/antenna-note';
 import { RelayRepository } from './repositories/relay';
-import { RegistryItem } from './entities/registry-item';
 import { ChannelRepository } from './repositories/channel';
-//mute
-import { ChannelFollowing } from './entities/channel-following';
-import { ChannelNotePining } from './entities/channel-note-pining';
-import { PasswordResetRequest } from './entities/password-reset-request';
 
 export const Announcements = getRepository(Announcement);
 export const AnnouncementReads = getRepository(AnnouncementRead);
@@ -85,7 +85,6 @@ export const UsedUsernames = getRepository(UsedUsername);
 export const Followings = getCustomRepository(FollowingRepository);
 export const FollowRequests = getCustomRepository(FollowRequestRepository);
 export const Instances = getRepository(Instance);
-//export const Emojis = getRepository(Emoji);
 export const Emojis = getCustomRepository(EmojiRepository);
 export const DriveFiles = getCustomRepository(DriveFileRepository);
 export const DriveFolders = getCustomRepository(DriveFolderRepository);
