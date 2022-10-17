742c63c092c09a8dde1e906215e4b9ae9495c4a0
yupix <yupi0982@outlook.jp>
2022-10-17 22:42:00 +0900

    feat: entitiesを読み込むように

--- packages/next-api/src/app.module.ts
+++ packages/next-api/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { TypeOrmModule } from '@ayuskey/nestjs-typeorm';
 import { config } from './const';
+import { entities } from './db/entities';
 
 @Module({
 	imports: [
@@ -14,7 +15,7 @@ import { config } from './const';
 			username: config.db.user,
 			password: config.db.pass,
 			database: config.db.db,
-			entities: [],
+			entities: entities,
 		}),
 		NotesModule,
 	],
