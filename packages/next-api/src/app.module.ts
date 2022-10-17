c3e72628d2087eb92bb3c8ecb1fb26b9ad002e69
yupix <yupi0982@outlook.jp>
2022-10-17 20:30:07 +0900

    chore: グローバル変数を追加

--- packages/next-api/src/app.module.ts
+++ packages/next-api/src/app.module.ts
@@ -1,15 +1,9 @@
-import * as yaml from 'js-yaml';
-import { readFileSync } from 'node:fs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { TypeOrmModule } from '@ayuskey/nestjs-typeorm';
-import { Config } from '../../../built/config/types';
-
-const config = yaml.load(
-	readFileSync('../../.config/default.yml', 'utf-8'),
-) as Config;
+import { config } from './const';
 
 @Module({
 	imports: [
@@ -20,7 +14,7 @@ const config = yaml.load(
 			username: config.db.user,
 			password: config.db.pass,
 			database: config.db.db,
-			entities: ['../../../built/models/entities/**/*.js'],
+			entities: [],
 		}),
 		NotesModule,
 	],
