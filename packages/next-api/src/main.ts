5bd565319d5c1fc0f903a2602ec5fe61c030b040
yupix <yupi0982@outlook.jp>
2022-10-18 15:33:22 +0900

    feat: ドキュメントを公開するように

--- packages/next-api/src/main.ts
+++ packages/next-api/src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from './const';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { RedocOptions, RedocModule } from '@nicholas.braun/nestjs-redoc'
 
 // TODO: まともにする
 const port = config.port + 100;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('/api/v2')
+	const options = new DocumentBuilder()
+		.setTitle('Ayuskey Next API')
+		.build();
+	const document = SwaggerModule.createDocument(app, options);
+	const redocOptions: RedocOptions = {
+		title: 'Ayuskey Next API Doc',
+		sortPropsAlphabetically: true,
+		hideDownloadButton: false,
+		hideHostname: false,
+
+	};
+	SwaggerModule.setup('/docs', app, document)
+	await RedocModule.setup('/redoc', app, document, redocOptions);
 	console.log(`port: ${port}`);
 	await app.listen(port);
 }
