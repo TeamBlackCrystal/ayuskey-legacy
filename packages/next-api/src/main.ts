c4e391c17b17ef7a3ba92e1f6d535b1ae2ee5c7e
yupix <yupi0982@outlook.jp>
2022-10-19 14:55:27 +0900

    chore: Loggerを使うように

--- packages/next-api/src/main.ts
+++ packages/next-api/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { config } from './const';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { RedocOptions, RedocModule } from '@nicholas.braun/nestjs-redoc'
+import { Logger } from 'nestjs-pino';
 
 // TODO: まともにする
 const port = config.port + 100;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useLogger(app.get(Logger));
 	app.setGlobalPrefix('/api/v2')
 	const options = new DocumentBuilder()
 		.setTitle('Ayuskey Next API')
