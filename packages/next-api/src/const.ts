e3d46b422a8fbd5ec4ec2bc6e3cff08c17d1dd78
yupix <yupi0982@outlook.jp>
2022-10-19 14:55:10 +0900

    chore: redisの設定を共通化

--- packages/next-api/src/const.ts
+++ packages/next-api/src/const.ts
@@ -1,3 +1,4 @@
+import { tryCreateUrl } from '@ayuskey/shared';
 import * as yaml from 'js-yaml';
 import { readFileSync } from 'node:fs';
 import type { Config } from '../../../built-dts/config/types';
@@ -5,3 +6,15 @@ import type { Config } from '../../../built-dts/config/types';
 export const config = yaml.load(
 	readFileSync('../../.config/default.yml', 'utf-8'),
 ) as Config;
+
+const url = tryCreateUrl(config.url);
+
+export const redisConfig = {
+	path: config.redis.path,
+    host: config.redis.host,
+    port: config.redis.port,
+    password: config.redis.pass,
+    db: config.redis.db,
+    family: config.redis.family,
+    keyPrefix: `${config.redis.prefix ? config.redis.prefix : url.host}:`
+}
--- packages/next-api/src/redis.ts
+++ packages/next-api/src/redis.ts
@@ -1,15 +1,4 @@
 import Redis from 'ioredis';
-import { config } from './const';
-import {tryCreateUrl} from '../../../built/config'
+import { redisConfig } from './const';
 
-const url = tryCreateUrl(config.url);
-
-export const redisClient = new Redis({
-    path: config.redis.path,
-    host: config.redis.host,
-    port: config.redis.port,
-    password: config.redis.pass,
-    db: config.redis.db,
-    family: config.redis.family,
-    keyPrefix: `${config.redis.prefix ? config.redis.prefix : url.host}:`
-})
+export const redisClient = new Redis(redisConfig)
