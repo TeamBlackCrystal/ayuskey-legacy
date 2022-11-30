4258de8d88687b7455bd903e3596b2dcb20c64e9
yupix <yupi0982@outlook.jp>
2022-12-01 01:21:37 +0900

    feat: apiにayuskey.js を使うように #AY-409

---
+++ src/client/app/api.ts
@@ -0,0 +1,8 @@
+import * as Ayuskey from 'ayuskey.js';
+import { $i } from './account';
+import * as config from './config';
+
+export const api = new Ayuskey.api.APIClient({
+	origin: config.url,
+	credential: $i
+})
