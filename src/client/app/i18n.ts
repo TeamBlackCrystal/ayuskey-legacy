fb7bbe6986c7bb6d3956b18653eb24781a35da47
yupix <yupi0982@outlook.jp>
2022-12-01 01:23:39 +0900

    feat: i18n用の新しい関数を追加

--- src/client/app/i18n.ts
+++ src/client/app/i18n.ts
@@ -1,9 +1,9 @@
-import { lang, locale } from './config';
+import { lang, locale } from "./config";
 
-export default function(scope?: string) {
+export default function (scope?: string) {
 	const texts = scope ? locale[scope] || {} : {};
-	texts['@'] = locale['common'];
-	texts['@deck'] = locale['deck'];
+	texts["@"] = locale["common"];
+	texts["@deck"] = locale["deck"];
 	return {
 		sync: false,
 		locale: lang,
@@ -11,4 +11,24 @@ export default function(scope?: string) {
 			[lang]: texts,
 		},
 	};
+}
+
+export function i18n(scope?: string) {
+	let texts = scope ? locale[scope] || {} : {};
+	texts["@"] = locale["common"];
+	texts = { ...texts, ...locale["deck"] };
+	const t = (path: string) => {
+		const split_path = path.split(".");
+		if (split_path.length === 0) return texts[`${path}`];
+		let data = null;
+		for (const _path of split_path) {
+			if (data === null) {
+				data = texts[`${_path}`];
+			} else {
+				data = data[`${path}`];
+			}
+		}
+		return data;
+	};
+	return { t };
 }
