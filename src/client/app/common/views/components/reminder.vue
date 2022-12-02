03e686b4b777e66e9079ca1a0c92f7c99e519b0c
yupix <yupi0982@outlook.jp>
2022-12-02 17:37:20 +0900

    feat: signup周りの型を正確に

--- src/client/app/common/views/components/reminder.vue
+++ src/client/app/common/views/components/reminder.vue
@@ -1,50 +1,61 @@
 <template>
-<form class="mk-reminder" :class="{ reminderg }" @submit.prevent="onSubmit">
-	<ui-input v-model="username" type="text" pattern="^[a-zA-Z0-9_]+$" spellcheck="false" required>
-		<span>{{ $t('username') }}</span>
-		<template #prefix>@</template>
-	</ui-input>
-	<ui-input v-model="email" type="text" spellcheck="false" required>
-		<span>{{ $t('email') }}</span>
-	</ui-input>
-	<p class="desc">
-		{{ $t('desc') }}
-	</p>
-	<ui-button type="submit" :disabled="reminderg">{{ $t('submit') }}</ui-button>
-</form>
+	<form class="mk-reminder" :class="{ reminderg }" @submit.prevent="onSubmit">
+		<ui-input
+			v-model="username"
+			type="text"
+			pattern="^[a-zA-Z0-9_]+$"
+			spellcheck="false"
+			required
+		>
+			<span>{{ i18n.t("username") }}</span>
+			<template #prefix>@</template>
+		</ui-input>
+		<ui-input v-model="email" type="text" spellcheck="false" required>
+			<span>{{ i18n.t("email") }}</span>
+		</ui-input>
+		<p class="desc">
+			{{ i18n.t("desc") }}
+		</p>
+		<ui-button type="submit" :disabled="reminderg">{{
+			i18n.t("submit")
+		}}</ui-button>
+	</form>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import i18n from '../../../i18n';
-export default Vue.extend({
-	i18n: i18n('common/views/components/reminder.vue'),
-	data() {
-		return {
-			reminderg: false,
-			username: '',
-			email: '',
-		};
-	},
-	methods: {
-		onSubmit() {
-			this.reminderg = true;
-			this.$root.api('request-reset-password', {
-				username: this.username,
-				email: this.email,
-			}).then(res => {
-				this.$root.dialog({
-					type: 'success',
-					splash: true
-				});
-				this.$emit('done');
-			}).catch(() => {
-				alert(this.$t('failed'));
-				this.reminderg = false;
-			});
-		},
-	}
-});
+<script setup lang="ts">
+import { ref } from "vue";
+import { api } from "../../../api";
+import { i18n as _i18n } from "../../../i18n";
+
+const i18n = _i18n("common/views/components/reminder.vue");
+
+const reminderg = ref(false);
+const username = ref("");
+const email = ref("");
+
+const emit = defineEmits(["done"]);
+
+function onSubmit() {
+	reminderg.value = true;
+	api
+		.request("request-reset-password", {
+			params: {
+				username: username.value,
+				email: email.value,
+			},
+		})
+		.then((res) => {
+			this.$root.dialog({
+				type: "success",
+				splash: true,
+			}); // TODO: AY-413
+			emit("done");
+		})
+		.catch(() => {
+			alert(i18n.t("failed"));
+			reminderg.value = false;
+		});
+}
 </script>
 
 <style lang="stylus" scoped>
