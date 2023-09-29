7a861d8bee03b639112025efdda03a6d7e32d4cd
yupix <yupi0982@outlook.jp>
2023-09-29 22:08:06 +0000

    feat: ノートのキューが100万以下の場合にのみジョブを追加できるように

--- src/migration/userAfterHook.ts
+++ src/migration/userAfterHook.ts
@@ -11,28 +11,35 @@ import { migrateDriveFiles, migrateDriveFolders } from "./drive";
 import { getConnection } from "typeorm";
 import { migratePasswordResetRequests } from "./PassworResetRequest";
 import { migrateAuthSessions } from "./AuthSession";
-import { userAfterHookQueue } from "./jobqueue";
+import { noteQueue, userAfterHookQueue } from "./jobqueue";
 import { logger } from "./common";
 
 export async function userAfterHook(user: User) {
-    const originalDb = getConnection();
-    const nextDb = getConnection("nextDb");
-    await migrateDriveFolders(originalDb, nextDb, user.id);  // そんなにフォルダがあるなんてこと無いだろうから、キューにする必要はない?
-    await migrateDriveFiles(originalDb, nextDb, user.id);
-    await migrateNotes(originalDb, nextDb, user.id);
+	const originalDb = getConnection();
+	const nextDb = getConnection("nextDb");
+	await migrateDriveFolders(originalDb, nextDb, user.id); // そんなにフォルダがあるなんてこと無いだろうから、キューにする必要はない?
+	await migrateDriveFiles(originalDb, nextDb, user.id);
+	while (true) {
+		if ((await noteQueue.getCompletedCount()) < 1000000) {
+			await migrateNotes(originalDb, nextDb, user.id);
+            break
+		} else {
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+		}
+	}
 
-    await migrateUserKeypair(originalDb, nextDb, user.id);
-    await migrateUserPublickey(originalDb, nextDb, user.id);
-    await migrateUserSecurityKeys(originalDb, nextDb, user.id);
-    await migrateBlockings(originalDb, nextDb, user.id);
-    await migrateMutings(originalDb, nextDb, user.id);
-    await migrateFollowings(originalDb, nextDb, user.id);
-    await migrateSignins(originalDb, nextDb, user.id);
-    await migratePasswordResetRequests(originalDb, nextDb, user.id);
-    await migrateAuthSessions(originalDb, nextDb, user.id);
+	await migrateUserKeypair(originalDb, nextDb, user.id);
+	await migrateUserPublickey(originalDb, nextDb, user.id);
+	await migrateUserSecurityKeys(originalDb, nextDb, user.id);
+	await migrateBlockings(originalDb, nextDb, user.id);
+	await migrateMutings(originalDb, nextDb, user.id);
+	await migrateFollowings(originalDb, nextDb, user.id);
+	await migrateSignins(originalDb, nextDb, user.id);
+	await migratePasswordResetRequests(originalDb, nextDb, user.id);
+	await migrateAuthSessions(originalDb, nextDb, user.id);
 
-    if (await userAfterHookQueue.getCompletedCount() > 1000) {
-        await userAfterHookQueue.clean(0, "completed");
-    }
-    logger.succ(`UserAfterHook: ${user.id} の処理が完了しました`);
+	if ((await userAfterHookQueue.getCompletedCount()) > 1000) {
+		await userAfterHookQueue.clean(0, "completed");
+	}
+	logger.succ(`UserAfterHook: ${user.id} の処理が完了しました`);
 }
