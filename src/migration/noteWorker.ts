1be5453798eed2959f8dd8b1b3292cb9401279f4
yupix <yupi0982@outlook.jp>
2023-09-30 03:05:11 +0000

    chore: clusterを使うように

--- src/migration/noteWorker.ts
+++ src/migration/noteWorker.ts
@@ -1,34 +1,42 @@
 import { initDb } from "@/db/postgre";
 import { logger } from "./common";
-import {
-	noteQueue,
-} from "./jobqueue";
+import { noteQueue } from "./jobqueue";
 import noteProcessor from "./processor/note.processor";
 import { createConnection } from "typeorm";
 import config from "@/config";
 import { AyuskeyNextEntities } from "@/v13/models";
+import cluster from "cluster";
 
 async function main() {
-    await initDb();
-    await createConnection({
-		name: "nextDb",
-		type: "postgres",
-		host: config.db.nextDb.host,
-		port: config.db.nextDb.port,
-		username: config.db.nextDb.user,
-		password: config.db.nextDb.pass,
-		database: config.db.nextDb.db,
-		entities: AyuskeyNextEntities,
-	});
+	const numWorkers = 8;
 
-	noteQueue.process(noteProcessor);
+	if (cluster.isPrimary) {
+		for (let i = 0; i < numWorkers; i++) {
+			cluster.fork();
+		}
+		cluster.on("exit", (worker, code, signal) => {
+			console.log(`worker ${worker.process.pid} died`);
+		});
+	} else {
+		await initDb();
+		await createConnection({
+			name: "nextDb",
+			type: "postgres",
+			host: config.db.nextDb.host,
+			port: config.db.nextDb.port,
+			username: config.db.nextDb.user,
+			password: config.db.nextDb.pass,
+			database: config.db.nextDb.db,
+			entities: AyuskeyNextEntities,
+		});
 
-
-	noteQueue.on("completed", (job) => {
-		logger.succ(`Note: ${job.data.id} の処理が完了しました`);
-	});
-
+		noteQueue.process(noteProcessor);
+		noteQueue.on("completed", (job) => {
+			logger.succ(`Note: ${job.data.id} の処理が完了しました`);
+		});
+	}
 }
+
 main().catch((e) => {
 	console.warn(e);
 	process.exit(1);
