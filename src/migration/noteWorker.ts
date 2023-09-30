bfce91403f2beee73665477a0b5638231dc17ab9
yupix <yupi0982@outlook.jp>
2023-09-30 03:09:37 +0000

    chore: clusterをrequireでインポートするように

--- src/migration/noteWorker.ts
+++ src/migration/noteWorker.ts
@@ -5,32 +5,40 @@ import noteProcessor from "./processor/note.processor";
 import { createConnection } from "typeorm";
 import config from "@/config";
 import { AyuskeyNextEntities } from "@/v13/models";
-import cluster from "cluster";
 
-const numWorkers = 8;
+const cluster = require('cluster');
 
-if (cluster.isPrimary) {
-	for (let i = 0; i < numWorkers; i++) {
-		cluster.fork();
+async function main() {
+	const numWorkers = 8;
+
+	if (cluster.isPrimary) {
+		for (let i = 0; i < numWorkers; i++) {
+			cluster.fork();
+		}
+		cluster.on("exit", (worker: any, code :any, signal:any) => {
+			console.log(`worker ${worker.process.pid} died`);
+		});
+	} else {
+		await initDb();
+		await createConnection({
+			name: "nextDb",
+			type: "postgres",
+			host: config.db.nextDb.host,
+			port: config.db.nextDb.port,
+			username: config.db.nextDb.user,
+			password: config.db.nextDb.pass,
+			database: config.db.nextDb.db,
+			entities: AyuskeyNextEntities,
+		});
+
+		noteQueue.process(noteProcessor);
+		noteQueue.on("completed", (job) => {
+			logger.succ(`Note: ${job.data.id} の処理が完了しました`);
+		});
 	}
-	cluster.on("exit", (worker, code, signal) => {
-		console.log(`worker ${worker.process.pid} died`);
-	});
-} else {
-	initDb();
-	createConnection({
-		name: "nextDb",
-		type: "postgres",
-		host: config.db.nextDb.host,
-		port: config.db.nextDb.port,
-		username: config.db.nextDb.user,
-		password: config.db.nextDb.pass,
-		database: config.db.nextDb.db,
-		entities: AyuskeyNextEntities,
-	});
-
-	noteQueue.process(noteProcessor);
-	noteQueue.on("completed", (job) => {
-		logger.succ(`Note: ${job.data.id} の処理が完了しました`);
-	});
 }
+
+main().catch((e) => {
+	console.warn(e);
+	process.exit(1);
+});
